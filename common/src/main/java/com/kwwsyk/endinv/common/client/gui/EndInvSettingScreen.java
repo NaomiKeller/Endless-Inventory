179bfa01f70c9ab93f9bbc0ee324e23821b9aa5d
Kay Zhang <ocn.keyest@gmail.com>
2025-08-13 16:30:29 +0800

    AccessTransformer test

--- common/src/main/java/com/kwwsyk/endinv/common/client/gui/EndInvSettingScreen.java
+++ common/src/main/java/com/kwwsyk/endinv/common/client/gui/EndInvSettingScreen.java
@@ -267,9 +267,9 @@ public class EndInvSettingScreen extends Screen {
             Font font = EndInvSettingScreen.this.font;
             guiGraphics.drawString(font,tip,leftPos+CONFIG_ENTRY_X_OFFSET,widgetY,0xFFFFFF00);
             Component v = Component.literal(info.get()!=null? info.get().toString():"null");
-            int infoLength = font.width(v.getVisualOrderText());
+            int infoLength = Math.min(font.width(v.getVisualOrderText()), 100);
             //guiGraphics.drawString(font,v,widgetMidX-infoLength/2,widgetY,0xFF00FFFF);
-            AbstractWidget.renderScrollingString(guiGraphics,font,v,widgetMidX-infoLength/2,widgetY,widgetMidX+infoLength/2,widgetY,0xFF00FFFF);
+            AbstractWidget.renderScrollingString(guiGraphics,font,v,widgetMidX-infoLength/2,widgetY,widgetMidX+infoLength/2,widgetY+7,0xFF00FFFF);
         }
 
         @Override
