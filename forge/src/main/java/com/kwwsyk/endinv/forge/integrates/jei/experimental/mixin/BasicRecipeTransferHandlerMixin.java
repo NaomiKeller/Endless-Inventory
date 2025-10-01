4acbeb4b5b3fb41b3c2590ca847c2d6cf0a55f45
Kay Zhang <ocn.keyest@gmail.com>
2025-10-01 18:06:29 +0800

    pre-2; added SMAC; will add config screen.

---
+++ forge/src/main/java/com/kwwsyk/endinv/forge/integrates/jei/experimental/mixin/BasicRecipeTransferHandlerMixin.java
@@ -0,0 +1,9 @@
+package com.kwwsyk.endinv.forge.integrates.jei.experimental.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(targets = "mezz/jei/library/transfer/BasicRecipeTransferHandler")
+public class BasicRecipeTransferHandlerMixin
+{
+
+}
